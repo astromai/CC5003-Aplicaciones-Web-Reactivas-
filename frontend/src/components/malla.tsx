d038d544512db5104909a4b7687d799d869a5993
astromai <ubilla.tomas012@gmail.com>
2025-09-22 11:40:40 -0300

    feat: add feature to render information from backend

--- frontend/src/components/malla.tsx
+++ frontend/src/components/malla.tsx
@@ -1,9 +1,23 @@
 import type { Ramo } from "./ramo";
+import axios from "axios";
 import SemestreDisplay from "./semestre"; 
+import { useState, useEffect } from "react";
 
 export default function Malla() {
 
-  // Datos de prueba, para probar su visualización por ahora
+  const [semestres, setSemestres] = useState<{ id: number, titulo: string; ramos: Ramo[] }[]>([]);
+  // render data from backend
+  useEffect(() => {
+    axios.get("http://localhost:3000/semestres")
+      .then(res => {
+        setSemestres(res.data);
+        console.log(res.data);
+      })
+      .catch(err => console.error(err));
+  }, []); // <- array vacío asegura que solo se ejecute una vez al montar
+
+
+  /*
   const semestres: { titulo: string; ramos: Ramo[] }[] = [
     {
       titulo: "Semestre 1",
@@ -30,11 +44,12 @@ export default function Malla() {
       ],
     },
   ];
+  */
 
   return (
     <div style={{ display: "flex", gap: 24, padding: 24, flexWrap: "wrap" }}>
-      {semestres.map((sem, i) => (
-        <SemestreDisplay key={i} titulo={sem.titulo} ramos={sem.ramos} />
+      {semestres.map((sem) => (
+        <SemestreDisplay key={sem.id} titulo={sem.titulo} ramos={sem.ramos} />
       ))}
     </div>
   );
